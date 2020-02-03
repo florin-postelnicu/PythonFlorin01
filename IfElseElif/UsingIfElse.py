c1824548bbcdf0855705803753bb182c395221fa
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2020-02-03 12:41:28 -0700

    Add files via upload

---
+++ IfElseElif/UsingIfElse.py
@@ -0,0 +1,11 @@
+# Decide if a number is negative, positive, or equal to zero
+print("Enter a number")
+number = float(input(" Enter your number  "))
+
+
+if number < 0 :
+    print("The number is negative :  ",  number)
+elif number > 0 :
+    print("The number is positive :", number)
+else:
+    print("The number is zero :" , number )
